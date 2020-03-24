8b1de6b370570027c59ce137aa9df47a66e423f1
Akram Noui <im_noui@esi.dz>
2020-03-24 19:26:33 +0100

    intersection first part

--- TopoGiraffe/MainWindow.xaml.cs
+++ TopoGiraffe/MainWindow.xaml.cs
@@ -33,6 +33,8 @@ namespace TopoGiraffe
         Polyline courbeActuelle;
         
         List<Ellipse> cercles = new List<Ellipse>();
+        PolyLineSegment polylinesegment = new PolyLineSegment();
+        
 
 
 
@@ -196,6 +198,101 @@ namespace TopoGiraffe
             if (activerDessinCheckBox.IsChecked == true)
             {
                 mainCanvas.Cursor = Cursors.Cross;
+            }
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+
+            PathGeometry myPathGeometry = new PathGeometry();
+            PathFigure pathFigure2 = new PathFigure();
+            PolyLineSegment myPolyLineSegment = new PolyLineSegment();
+            myPolyLineSegment.Points = polylines[1].Points;
+            pathFigure2.Segments.Add(myPolyLineSegment);
+            myPathGeometry.Figures.Add(pathFigure2);
+
+
+            // line
+            PathGeometry myPathGeometry1 = new PathGeometry();
+            PathFigure pathFigure3 = new PathFigure();
+            PolyLineSegment myPolyLineSegment1 = new PolyLineSegment();
+            myPolyLineSegment1.Points = polylines[0].Points;
+            pathFigure3.Segments.Add(myPolyLineSegment1);
+            myPathGeometry1.Figures.Add(pathFigure3);
+            VisualTreeHelper.HitTest(polylines[0], null, new HitTestResultCallback(HitTestCallback), new GeometryHitTestParameters(myPathGeometry));
+     
+            MessageBox.Show("hey :" +hits.Count);
+         
+        }
+        private List<DrawingVisual> hits = new List<DrawingVisual>();
+       
+
+        private HitTestResultBehavior HitTestCallback(HitTestResult result)
+        {
+            GeometryHitTestResult geometryResult = (GeometryHitTestResult)result;
+             DrawingVisual visual = result.VisualHit as DrawingVisual;
+            // Only include matches that are DrawingVisual objects and
+            // that are completely inside the geometry.
+            MessageBox.Show("heyyouu");
+
+            if (visual != null &&
+            geometryResult.IntersectionDetail == IntersectionDetail.Intersects)
+            {
+                hits.Add(visual);
+            }
+            return HitTestResultBehavior.Continue;
+        }
+        private List<object> hitResultsList = new List<object>();
+
+        
+
+        private void mainCanvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            PathGeometry myPathGeometry = new PathGeometry();
+            PathFigure pathFigure2 = new PathFigure();
+            PolyLineSegment myPolyLineSegment = new PolyLineSegment();
+            myPolyLineSegment.Points = polylines[1].Points;
+            pathFigure2.Segments.Add(myPolyLineSegment);
+            myPathGeometry.Figures.Add(pathFigure2);
+
+
+            // Retrieve the coordinate of the mouse position.
+            Point pt = e.GetPosition((UIElement)sender);
+
+            // Clear the contents of the list used for hit test results.
+            hitResultsList.Clear();
+
+            // Set up a callback to receive the hit test result enumeration.
+            VisualTreeHelper.HitTest(polylines[0], new HitTestFilterCallback(MyHitTestFilter),
+                new HitTestResultCallback(MyHitTestResult),
+                new GeometryHitTestParameters(myPathGeometry));
+
+            // Perform actions on the hit test results list.
+            if (hitResultsList.Count >= 0)
+            {
+                MessageBox.Show("Number of Visuals Hit: " + hitResultsList.Count);
+            }
+        }
+        public HitTestResultBehavior MyHitTestResult(HitTestResult result)
+        {
+            // Add the hit test result to the list that will be processed after the enumeration.
+            hitResultsList.Add(result.VisualHit);
+
+            // Set the behavior to return visuals at all z-order levels.
+            return HitTestResultBehavior.Stop;
+        }
+        public HitTestFilterBehavior MyHitTestFilter(DependencyObject o)
+        {
+            // Test for the object value you want to filter.
+            if (o.GetType() == typeof(Canvas))
+            {
+                // Visual object and descendants are NOT part of hit test results enumeration.
+                return HitTestFilterBehavior.Continue;
+            }
+            else
+            {
+                // Visual object is part of hit test results enumeration.
+                return HitTestFilterBehavior.Continue;
             }
         }
     }
---
+++ TopoGiraffe/intersection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopoGiraffe
+{
+    public class intersection
+    {
+
+
+
+
+
+
+
+
+    }
+}
