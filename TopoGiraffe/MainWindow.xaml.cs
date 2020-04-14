cd82923e0bc2dc1e2bc50b35458ee23fc38b3614
Akram Noui <im_noui@esi.dz>
2020-04-14 02:23:32 +0100

    Saisie d'Altitudes

--- TopoGiraffe/MainWindow.xaml.cs
+++ TopoGiraffe/MainWindow.xaml.cs
@@ -39,6 +39,7 @@ namespace TopoGiraffe
         Polyline poly = new Polyline();
         int LinePointscpt = 0;
         Boolean firstPoint = true;
+        List<int> Altitudes = new List<int>();
 
         List<List<ArtPoint>> PointsGlobal = new List<List<ArtPoint>>();
         int nbCourbes = 0;
@@ -332,8 +333,9 @@ namespace TopoGiraffe
             finalCtrlPoint = false;
 
 
+            OpenCourbeinfo();
 
-            
+
 
 
 
@@ -1008,6 +1010,9 @@ namespace TopoGiraffe
                             DragPoints = PointsGlobal[polylines.IndexOf(polyline)];
                         }
                     }
+                    int i = polylines.IndexOf((Polyline) selectedPolyline);
+                    AltitudeLabel.Visibility = Visibility.Visible;
+                    AltitudeLabel.Content = Altitudes[i];
                 }
                 else return;
 
@@ -1109,6 +1114,7 @@ namespace TopoGiraffe
                 }
             }
         }
+        int indexAltitude = 0;
 
 
         //private void mainCanvas_MouseUp(object sender, MouseButtonEventArgs e)
@@ -1137,6 +1143,27 @@ namespace TopoGiraffe
 
 
             }
+
+        }
+
+       
+
+        public void OpenCourbeinfo()
+        {
+            Window1 Window1 = new Window1();
+
+            Window1.ShowDialog();
+            if (Window1.DialogResult == true)
+            {
+               
+
+                Altitudes.Add(Convert.ToInt32(Window1.Altitude.Text));
+               // StyleCmbToRealStyle(courbeActuelle,Convert.ToInt32(Window1.Type.SelectedIndex));
+                indexAltitude++;
+
+            }
+
+
 
         }
 
