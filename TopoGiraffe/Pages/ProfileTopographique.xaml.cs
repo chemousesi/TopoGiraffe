5f76693935fbca735c5cec646d82c5ae19a88318
Alaeddine <ia_nasri@esi.dz>
2020-05-17 02:22:16 +0200

    point's tooltip styling

--- TopoGiraffe/Pages/ProfileTopographique.xaml.cs
+++ TopoGiraffe/Pages/ProfileTopographique.xaml.cs
@@ -64,6 +64,7 @@ namespace TopoGiraffe
                  //   PointGeometrySize = 4,
                     AreaLimit = 0,
                     LineSmoothness = 0.4,
+                    
                     //DataLabels = true,
                 }
             };
@@ -80,6 +81,7 @@ namespace TopoGiraffe
         public string[] Labels { get; set; }
         public Func<double, string> YFormatter { get; set; }
         public ChartValues<ObservablePoint> MyValues { get; set; }
+  
 
         public void SaveToPng(FrameworkElement visual, string fileName)
         {
