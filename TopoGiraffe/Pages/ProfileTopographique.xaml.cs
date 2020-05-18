deee4ca8b2dc68ab5eefa5d6f290e3c3448f8e46
Alaeddine <ia_nasri@esi.dz>
2020-05-18 17:30:22 +0200

    Commenting profile topographique page

--- TopoGiraffe/Pages/ProfileTopographique.xaml.cs
+++ TopoGiraffe/Pages/ProfileTopographique.xaml.cs
@@ -18,17 +18,17 @@ namespace TopoGiraffe
     /// </summary>
     public partial class ProfileTopographique : Window
     {
-        List<IntersectionDetail> IntersectionPoints;
-        List<double> distancesListe;
+        List<IntersectionDetail> IntersectionPoints;   //La liste des points d'intersection
+        List<double> distancesListe;  //La liste des deistances entre chaque deux points 
         Echelle mainScale;
         String penteText;
 
         public ProfileTopographique(List<IntersectionDetail> aIntersectionPoints, List<double> adistancesList, Echelle mainScale, String pente)
         {
-            this.IntersectionPoints = aIntersectionPoints;
+            this.IntersectionPoints = aIntersectionPoints; 
             // this.distancesListe = adistancesList;
-            this.mainScale = mainScale;
-            this.penteText = pente;
+            this.mainScale = mainScale;    //L'échelle des x 
+            this.penteText = pente;        // Creation du dailogebox du pente
             Resources.Clear();
 
             Resources.MergedDictionaries.Clear();
@@ -39,32 +39,29 @@ namespace TopoGiraffe
 
             InitializeComponent();
 
-            penteTextBox.Text = penteText;
-#pragma warning disable CS0168 // La variable 'compteur' est déclarée, mais jamais utilisée
-            int compteur;
-#pragma warning restore CS0168 // La variable 'compteur' est déclarée, mais jamais utilisée
+            penteTextBox.Text = penteText; // L'affichage du pente 
             //Echelle scale = new Echelle(130, 1000);
-            MyValues = new ChartValues<ObservablePoint>();
+            MyValues = new ChartValues<ObservablePoint>(); //Creation de la liste des valeurs 
 
 
 
 
-            foreach (IntersectionDetail IntersectionPoint in IntersectionPoints)
+            foreach (IntersectionDetail IntersectionPoint in IntersectionPoints) //Boucle pour parcourir la liste des points d'intersections
             {
-                double distance = System.Math.Round(mainScale.FindDistanceOnField(IntersectionPoint.distance), 3);
-                MyValues.Add(new ObservablePoint(distance, IntersectionPoint.altitude));
+                double distance = System.Math.Round(mainScale.FindDistanceOnField(IntersectionPoint.distance), 3); 
+                MyValues.Add(new ObservablePoint(distance, IntersectionPoint.altitude)); // Afectation les valeurs a la liste des valeurs "les x et les y"
             }
-            SeriesCollection = new SeriesCollection
+            SeriesCollection = new SeriesCollection   //La creation de la courbe 
                 {
-                new LineSeries
+                new LineSeries 
                 {
-                    Title = " Profil Topgraphique",
+                    Title = " Profil Topgraphique", //Titre de graph
                    // Fill = Brushes.Red,
                    // StrokeThickness = 4,
-                    Values = MyValues,//les valeurs
+                    Values = MyValues,           // Afectation des valeurs au graph 
                  //   PointGeometrySize = 4,
-                    AreaLimit = 0,
-                    LineSmoothness = 0.4,
+                    AreaLimit = 0,      
+                    LineSmoothness = 0.4,    //Ajuster la finesse des lignes
                     
                     //DataLabels = true,
                 }
@@ -72,11 +69,14 @@ namespace TopoGiraffe
 
             DataContext = this;
         }
-        private void AddResourceDictionary(string src)
+        private void AddResourceDictionary(string src) 
         {
-            Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri(src, UriKind.Relative) });
+            Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri(src, UriKind.Relative) }); 
         }
 
+
+
+        // les variables externes
         public SeriesCollection SeriesCollection { get; set; }
         public LineSeries LineSeries { get; set; }
         public string[] Labels { get; set; }
@@ -84,13 +84,13 @@ namespace TopoGiraffe
         public ChartValues<ObservablePoint> MyValues { get; set; }
 
 
-        public void SaveToPng(FrameworkElement visual, string fileName)
+        public void SaveToPng(FrameworkElement visual, string fileName)  //Sauvgader le graph en image
         {
             var encoder = new PngBitmapEncoder();
             EncodeVisual(visual, fileName, encoder);
         }
 
-        private static void EncodeVisual(FrameworkElement visual, string fileName, BitmapEncoder encoder)
+        private static void EncodeVisual(FrameworkElement visual, string fileName, BitmapEncoder encoder) //Creation du fichier pdf
         {
             var bitmap = new RenderTargetBitmap((int)visual.ActualWidth, (int)visual.ActualHeight, 96, 96, PixelFormats.Pbgra32);
             bitmap.Render(visual);
@@ -99,7 +99,7 @@ namespace TopoGiraffe
             using (var stream = File.Create(fileName)) encoder.Save(stream);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)   //Sauvgarder le profile topographique en pdf
         {
             RenderTargetBitmap rtb = new RenderTargetBitmap((int)chart.ActualWidth, (int)chart.ActualHeight, 96, 96, PixelFormats.Pbgra32);
             rtb.Render(visual: chart);
@@ -107,9 +107,9 @@ namespace TopoGiraffe
             png.Frames.Add(BitmapFrame.Create(rtb));
             MemoryStream stream = new MemoryStream();
             png.Save(stream);
-            SaveToPng(chart, "MyChart.png");
+            SaveToPng(chart, "MyChart.png"); //le nom de l'image
 
-            iTextSharp.text.Document doc = new iTextSharp.text.Document(iTextSharp.text.PageSize.A3, 10, 40, 42, 35);
+            iTextSharp.text.Document doc = new iTextSharp.text.Document(iTextSharp.text.PageSize.A3, 10, 40, 42, 35); //les dimensions du fichier pdf
 
 
             Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
@@ -130,14 +130,15 @@ namespace TopoGiraffe
             }
             try
             {
-
                 PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream(filename, FileMode.Create));
             }
             catch (ArgumentNullException excp)
             {
-                MessageBox.Show("il faut specifier un fichier pdf");
+                MessageBox.Show("il faut specifier un fichier pdf"); //le cas de ne pas selectioner un fichier
             }
 
+
+            //L'exportation en pdf
 
 
             doc.Open();
@@ -147,14 +148,14 @@ namespace TopoGiraffe
             doc.Add(jpg);
             doc.Add(new iTextSharp.text.Paragraph("Original Width: "));
 
-            Viewbox viewbox = new Viewbox();
+            Viewbox viewbox = new Viewbox(); //Ajuster la photo dans un cadre
 
 
 
 
 
             doc.Close();
-            Export.IsEnabled = false;
+            Export.IsEnabled = false; //desactivation du button de l'exportation
         }
     }
 }
